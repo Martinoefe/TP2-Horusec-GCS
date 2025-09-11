c0931da0b9974de8842b731a69b2fb6f294c3b3f
martinoefe <martinofioramonti1@gmail.com>
2025-09-11 09:27:36 -0300

    Se agregan comentarios y se corrigen vulnerabilidades

--- core/src/com/chess/game/EjemploVulnerable.java
+++ core/src/com/chess/game/EjemploVulnerable.java
@@ -1,7 +1,7 @@
 package com.chess.game.utils;
 
-public class FakeCreds {
-    // ❌ Credenciales de AWS hardcodeadas (Horusec-leaks suele marcarlas CRITICAL)
+public class EjemploVulnerable {
+    // Credenciales de AWS hardcodeadas para que horusec las detecte como vulnerabilidades CRITICAL
     private static final String AWS_ACCESS_KEY_ID = "AKIAIOSFODNN7EXAMPLE";
     private static final String AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
 
